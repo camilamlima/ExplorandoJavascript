4024938d3c61a6aeb30e8e0b446bb760badbd7df
Camila Lima <camilam.oliveiralima@gmail.com>
2023-01-03 23:05:24 -0300

    Finalizando Explorando Javascript

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -8,19 +8,35 @@ const listaDeDestinos = new Array(
 
 const idadeComprador = 15;
 const estaAcompanhada = true;
+const passagemComprada = true;
 
 console.log("Destinos possíveis: ");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
-    console.log("Comprador maior de idade");
+// if(idadeComprador >= 18){
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(3,1); //removendo um item
+// }else if(estaAcompanhada == 18){
+//         console.log("Comprador esta acompanhado");
+//         listaDeDestinos.splice(3,1); //removendo um item
+// }else{
+//         console.log("Não é maior de Idade e não posso vender")
+// }
+
+if(idadeComprador >= 18 || estaAcompanhada == true){
+    console.log("Boa Viagem !!");
     listaDeDestinos.splice(3,1); //removendo um item
-}else if(estaAcompanhada){
-        console.log("Comprador esta acompanhado");
-        listaDeDestinos.splice(3,1); //removendo um item
 }else{
-        console.log("Não é maior de Idade e não posso vender")
+    console.log("Não é maior de Idade e não posso vender")
 }
+
+console.log("Embarque: \n\n")
+if(idadeComprador >=18 && passagemComprada){
+    console.log("Boa Viagem !!");
+}else{
+    console.log("Você não pode embarcar");
+}
+
 
 
 // console.log(idadeComprador > 18);
